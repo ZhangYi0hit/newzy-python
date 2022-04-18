67f38c832ebe9067e4012169ff008123b97d20f2
ZhangYi <76429622+ZhangYi0hit@users.noreply.github.com>
2022-04-18 23:54:37 +0800

    Update app.py

--- euro-to-usd/app.py
+++ euro-to-usd/app.py
@@ -13,15 +13,13 @@ def form():
     return render_template("form.html")
 
 
-@app.route("/hello")
-def form1():
-    return render_template("hello.html")
 
-@app.route("/", methods=["POST"])
+
+@app.route("/hello", methods=["POST"])
 def my_form_post():
     username=request.form["username"]
     pwd=request.form["pwd"]	
-    return redirect('/hello',username=username)
+    return render_template("hello.html",username=username)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
